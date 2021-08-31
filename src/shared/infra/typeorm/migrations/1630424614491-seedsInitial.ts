3d31af2ccb2bc3e053df87c4777f4d8ac65623d2
George Freitas <george.freitas@grupogreencard.com.br>
2021-08-31 16:01:39 +0000

    Ajuste migrations insert dados iniciais

--- src/shared/infra/typeorm/migrations/1630424614491-seedsInitial.ts
+++ src/shared/infra/typeorm/migrations/1630424614491-seedsInitial.ts
@@ -5,7 +5,7 @@ export class seedsInitial1630424614491 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`INSERT INTO "MOOVIN".account_types(id, description) VALUES (1, 'Conta corrente'),(2, 'Conta polpança');`);
         await queryRunner.query(`INSERT INTO "MOOVIN".transaction_types (id, description) VALUES (1, 'Deposito'), (2, 'Saque'), (3, 'Taxa de saque');`);
-        await queryRunner.query(`INSERT INTO "MOOVIN".accounts(id, "name", "accountTypeId", balance, "createdAt", "updatedAt") VALUES(1, 'Fulano', 1, 0, '2021-08-31 08:16:42.733', '2021-08-31 15:18:21.035');`);
+        await queryRunner.query(`INSERT INTO "MOOVIN".accounts(id, "name", "accountTypeId", balance, "created_at", "updated_at") VALUES(1, 'Fulano', 1, 0, '2021-08-31 08:16:42.733', '2021-08-31 15:18:21.035');`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
